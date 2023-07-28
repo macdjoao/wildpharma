9260cceb2da15c971da77e66254af71d6a4a351c
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-28 11:37:53 -0300

    feat: Esboço dos metodos de CustomerService

--- customers/services.py
+++ customers/services.py
@@ -1,4 +1,23 @@
-class CustomerService:
-    pass
+# Seguir nomenclatura CRUD
 
-    # TODO
+class CustomerService:
+
+    @staticmethod
+    def create():
+        return 'Create CustomerService'
+
+    @staticmethod
+    def read_one():
+        return 'Read One CustomerService'
+
+    @staticmethod
+    def read_many():
+        return 'Read Many CustomerService'
+
+    @staticmethod
+    def update():
+        return 'Update CustomerService'
+
+    @staticmethod
+    def delete():
+        return 'Delete CustomerService'
