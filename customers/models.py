b0cce83519c704722209dcf11adeb320fcdb06cf
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-28 12:54:51 -0300

    fix: Consertando deleted_at

--- customers/models.py
+++ customers/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=30, blank=False)
-    email = models.EmailField(unique=True, blank=False)
-    phone = models.CharField(max_length=11, unique=True, blank=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11, unique=True)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(default=None)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.email
