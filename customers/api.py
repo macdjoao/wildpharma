0ef6fa149c30729cee6028d346d96398bce15a3e
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-28 11:13:28 -0300

    feat: Esboço das rotas de Customers

--- customers/api.py
+++ customers/api.py
@@ -3,6 +3,31 @@ from ninja import Router
 router = Router(tags=['Customers'])
 
 
+@router.post('/')
+def post(request):
+    # TODO
+    return 'Post Customer'
+
+
 @router.get('/')
-def hello_world(request):
-    return 'Olá mundo'
+def get_many(request):
+    # TODO
+    return 'Get Many Customers'
+
+
+@router.get('/{customer_id}')
+def get_one(request):
+    # TODO
+    return 'Get One Customer'
+
+
+@router.patch('/{customer_id}')
+def patch(request):
+    # TODO
+    return 'Patch Customer'
+
+
+@router.delete('/{customer_id}')
+def delete(request):
+    # TODO
+    return 'Delete Customer'
