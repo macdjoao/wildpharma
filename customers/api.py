1d281962b2e01f68c8e2c41d067017c4e52f72df
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-28 15:54:24 -0300

    feat: Adicionando os Schemas de Customer

--- customers/api.py
+++ customers/api.py
@@ -1,35 +1,36 @@
 # Seguir nomenclatura HTTP
 
 from ninja import Router
+from customers.schemas import PostCustomerSchema, GetOneCustomerSchema, GetManyCustomersSchema, PatchCustomerSchema
 
 router = Router(tags=['Customers'])
 
 
-@router.post('/')
-def post(request):
+@router.post('/', response=GetOneCustomerSchema)
+def post(request, body: PostCustomerSchema):
     # TODO
     return 'Post Customer'
 
 
-@router.get('/')
+@router.get('/', response=GetManyCustomersSchema)
 def get_many(request):
     # TODO
     return 'Get Many Customers'
 
 
-@router.get('/{customer_id}')
+@router.get('/{customer_id}', response=GetOneCustomerSchema)
 def get_one(request, customer_id):
     # TODO
     return 'Get One Customer'
 
 
-@router.patch('/{customer_id}')
-def patch(request, customer_id):
+@router.patch('/{customer_id}', response=GetOneCustomerSchema)
+def patch(request, customer_id, body: PatchCustomerSchema):
     # TODO
     return 'Patch Customer'
 
 
-@router.delete('/{customer_id}')
+@router.delete('/{customer_id}', response=GetOneCustomerSchema)
 def delete(request, customer_id):
     # TODO
     return 'Delete Customer'
--- customers/schemas.py
+++ customers/schemas.py
@@ -1,24 +1,36 @@
 # Seguir nomenclatura HTTP
+# Por questões de segurança, usarei ModelSchema e Schema em vez de create_schema ( https://django-ninja.rest-framework.com/guides/response/django-pydantic-create-schema/ )
 
-class PostCustomer:
-    pass
-
-# TODO
-
-
-class GetCustomer:
-    pass
-
-# TODO
+from ninja import Schema
+from datetime import datetime
+from typing import List, Optional
 
 
-class PatchCustomer:
-    pass
+class PostCustomerSchema(Schema):
+    first_name: str
+    last_name: str
+    email: str
+    phone: str
 
-# TODO
+
+class GetOneCustomerSchema(Schema):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    phone: str
+    active: bool
+    created_at: datetime
+    updated_at: datetime = None
+    deleted_at: datetime = None
 
 
-class DeleteCustomer:
-    pass
+class GetManyCustomersSchema(Schema):
+    customers: List[GetOneCustomerSchema]
 
-# TODO
+
+class PatchCustomerSchema(Schema):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
