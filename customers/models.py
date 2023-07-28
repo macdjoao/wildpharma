5af007ac89d36438e996b83cf69cbeca6c790b0e
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-28 12:09:58 -0300

    feat: Customer Model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    phone = models.CharField(max_length=11, unique=True, blank=False)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(default=None)
+
+    def __str__(self) -> str:
+        return self.email
