9de39dd735dc0ab6620961b14a97053a18f2bc48
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-08-02 18:58:48 -0300

    fix: Erros method not allowed

--- customers/api.py
+++ customers/api.py
@@ -8,20 +8,9 @@ from typing import List
 router = Router(tags=['Customers'])
 
 
-@router.delete('/{customer_id}', summary='Delete customer', response=GetCustomerSchema)
-def delete_customer(request, customer_id: int):
-    # TODO
-    return CustomerService.delete(id=customer_id)
-
-
 @router.post('/', summary='Create customer', response=GetCustomerSchema)
 def post_customer(request, payload: PostCustomerSchema):
     return CustomerService.create(data=payload)
-
-
-@router.get('/{active}', summary='Get all customers', response=List[GetCustomerSchema])
-def get_many_customers(request, active: bool):
-    return CustomerService.read_many(active=active)
 
 
 @router.get('/{customer_id}/{active}', summary='Get one customer', response=GetCustomerSchema)
@@ -29,7 +18,17 @@ def get_one_customer(request, customer_id: int, active: bool):
     return CustomerService.read_one(id=customer_id, active=active)
 
 
+@router.get('/{active}', summary='Get all customers', response=List[GetCustomerSchema])
+def get_many_customers(request, active: bool):
+    return CustomerService.read_many(active=active)
+
+
 @router.patch('/{customer_id}', summary='Update customer', response=GetCustomerSchema)
 def patch_customer(request, customer_id: int, payload: PatchCustomerSchema):
-    # TODO
     return CustomerService.update(id=customer_id, data=payload)
+
+
+@router.delete('/{customer_id}', summary='Delete customer', response=GetCustomerSchema)
+def delete_customer(request, customer_id: int):
+    # TODO
+    return CustomerService.delete(id=customer_id)
