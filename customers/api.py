76bb18918ceac5505bdcfa96f7643fb19cb31588
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-08-04 16:30:30 -0300

    feat: Adicionando codigo http de resposta as rotas

--- customers/api.py
+++ customers/api.py
@@ -9,28 +9,28 @@ from ninja.pagination import paginate, LimitOffsetPagination
 router = Router(tags=['Customers'])
 
 
-@router.post('/', summary='Create customer', response=GetCustomerSchema)
+@router.post('/', summary='Create customer', response={201: GetCustomerSchema})
 def post_customer(request, payload: PostCustomerSchema):
     return CustomerService.create(data=payload)
 
 
-@router.get('/{customer_id}/{active}', summary='Get one customer', response=GetCustomerSchema)
+@router.get('/{customer_id}/{active}', summary='Get one customer', response={200: GetCustomerSchema})
 def get_one_customer(request, customer_id: int, active: bool):
     return CustomerService.read_one(id=customer_id, active=active)
 
 
-@router.get('/{active}', summary='Get all customers', response=List[GetCustomerSchema])
+@router.get('/{active}', summary='Get all customers', response={200: List[GetCustomerSchema]})
 @paginate(LimitOffsetPagination)
 def get_many_customers(request, active: bool):
     return CustomerService.read_many(active=active)
 
 
-@router.patch('/{customer_id}', summary='Update customer', response=GetCustomerSchema)
+@router.patch('/{customer_id}', summary='Update customer', response={200: GetCustomerSchema})
 def patch_customer(request, customer_id: int, payload: PatchCustomerSchema):
     return CustomerService.update(id=customer_id, data=payload)
 
 
-@router.delete('/{customer_id}', summary='Delete customer', response=GetCustomerSchema)
+@router.delete('/{customer_id}', summary='Delete customer', response={204: GetCustomerSchema})
 def delete_customer(request, customer_id: int):
     # TODO
     return CustomerService.delete(id=customer_id)
